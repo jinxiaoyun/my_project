3430ccbc637decd5d47a6ada60d5191618a51419
xiaoxiong <35299231@qq.com>
2017-11-25 22:37:04 +0800

    前端项目文件上传

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(function (req,res,next) {
   if(req.cookies.userId){
